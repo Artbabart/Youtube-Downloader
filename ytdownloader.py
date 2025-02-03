5e7fc3d91a3ce7ba703e03197c68968268f3ff6a
Julia Toth <153101953+Artbabart@users.noreply.github.com>
2025-02-03 22:54:00 +0100

    Update ytdownloader.py

--- ytdownloader.py
+++ ytdownloader.py
@@ -1,2 +1,14 @@
-from pytube import YouTube
-from sys import argv
+import yt_dlp
+
+url = "https://youtu.be/cxQxajcOyCI?si=P9nKGFSeGRgk08VD"
+
+ydl_opts = {
+    'format': 'best', 
+    'outtmpl': 'downloads/%(title)s.%(ext)s', 
+    'noplaylist': True
+}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
+
+print("Letöltés kész!")
