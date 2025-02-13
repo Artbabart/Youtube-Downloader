340f21975fe9a8d09fd48a716efc4fff9f2f0d89
toth julia <t.julia982@gmail.com>
2025-02-13 11:25:46 +0100

    :qa check if there is such a file, if there is, do not download it

--- ytdownloader.py
+++ ytdownloader.py
@@ -1,14 +1,26 @@
 import yt_dlp
+import os
 
-url = "https://youtu.be/cxQxajcOyCI?si=P9nKGFSeGRgk08VD"
+download_folder = "downloads"
+
+url = "https://www.youtube.com/watch?v=0QPq041UhkQ"
 
 ydl_opts = {
-    'format': 'best', 
-    'outtmpl': 'downloads/%(title)s.%(ext)s', 
+    'format': 'best',
+    'outtmpl': 'downloads/%(title)s.%(ext)s',
     'noplaylist': True
 }
+with yt_dlp.YoutubeDL({'quite': True}) as ydl:
+    info = ydl.extract_info(url, download=False)
+    video_title = info['title']
+    file_path = os.path.join(download_folder, f"{video_title}.mp4")
 
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([url])
+if os.path.exists(file_path):
+    print(f"Ez a videó már le van töltve: {file_path}")
+else:
+    print("A videó még nincs letöltve, elkezdem a letöltést...")
 
-print("Letöltés kész!")
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    
+    print(f"A videó sikeresen letöltve: {file_path}")
